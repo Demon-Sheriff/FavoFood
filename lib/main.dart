50fdf5ccd877198cdccffb8219010669e46f2cd4
DemonSheriff <anantshukla404@gmail.com>
2024-11-08 12:50:57 +0530

    added provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/tabs.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -11,7 +13,7 @@ final theme = ThemeData(
 );
 
 void main() {
-  runApp(const App());
+  runApp(const ProviderScope(child: App()));
 }
 
 class App extends StatelessWidget {
